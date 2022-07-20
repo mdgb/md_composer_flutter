bf073c70ebd290a77809aa12412bc7203e13f854
mdgb <marco.digiovambattista@gmail.com>
2022-07-20 09:08:42 +0200

    pod file per ios

--- lib/ui/pages/animations/animated_drawer_1.dart
+++ lib/ui/pages/animations/animated_drawer_1.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:md_composer_flutter/ui/pages/api_contents/gallery_page.dart';
+import 'package:md_composer_flutter/ui/pages/api_contents/posts_page.dart';
 import 'package:md_composer_flutter/ui/widget_library/drawers/drawer_expandable/drawer_expandable.dart';
 
 class AnimatedDrawer1 extends ConsumerStatefulWidget {
@@ -134,7 +135,7 @@ class _AnimatedDrawer1State extends ConsumerState<AnimatedDrawer1>
                     title: const Text('Animated Drawer'),
                   ),
                   body: Container(
-                    child: GalleryPage(),
+                    child: Posts(),
                   ),
                 ),
               ),
